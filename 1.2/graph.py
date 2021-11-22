0f97f1d989739cdebc4e47d8f51465a8802e09a4
Jakob Lund Johannessen <jakob-lj@hotmail.com>
2021-11-22 20:00:39 +0100

    feat: add labels to 1.2 and 1.3 plots

--- 1.2/graph.py
+++ 1.2/graph.py
@@ -1,6 +1,6 @@
 import matplotlib.pyplot as plt
 
-graphs = "1.3"
+graphs = "1.2"
 
 
 def formatLine(line):
@@ -9,9 +9,9 @@ def formatLine(line):
 
 def plotFile(plt, fileName):
     if ("in" in fileName):
-        legend = "Interface %s: In" % fileName.split(".")[-1]
+        legend = "Interface %s: ifInUcastPkts" % fileName.split(".")[-1]
     else:
-        legend = "Interface %s: Out" % fileName.split(".")[-1]
+        legend = "Interface %s: ifOutUcastPkts" % fileName.split(".")[-1]
     with open("data/%s/%s" % (graphs, fileName)) as f:
         data = f.readlines()
         attributes = [formatLine(line) for line in data]
@@ -21,11 +21,16 @@ def plotFile(plt, fileName):
         plt.plot(x, attributes, label=legend)
 
 
-plotFile(plt, "in.6")
-plotFile(plt, "in.7")
-plotFile(plt, "out.6")
-plotFile(plt, "out.7")
+#plotFile(plt, "in.6")
+#plotFile(plt, "in.7")
+#plotFile(plt, "out.6")
+#plotFile(plt, "out.7")
+plotFile(plt, "in.5")
+plotFile(plt, "out.5")
 
+plt.title("Reuqests per 5 seconds")
+plt.xlabel("Time [sec]")
+plt.ylabel("Number of requests per 5 sec")
 plt.legend()
 plt.savefig("graphs/" + graphs + ".png")
 plt.show()
